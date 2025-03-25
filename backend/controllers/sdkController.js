a154ad0cbfbd7bfb382875d85ff3530e9d1aab80
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-03-25 18:26:07 +0530

    Implement country tracking by fetching country name and sending it to the backend API

--- backend/controllers/sdkController.js
+++ backend/controllers/sdkController.js
@@ -3,13 +3,13 @@ const Analytics = require("../models/analytics");
 // Controller to handle posting the countryName
 exports.postCountryName = async (req, res) => {
     try {
-        const { payload } = req.body;
-        // const newAnalytics = new Analytics({
-        //     countryName,
-        // });
-        // await newAnalytics.save();
-        console.log(`Received countryName: ${payload}`);
-        res.status(200).json({ message: "Analytics received successfully", payload });
+        const { countryName } = req.body;
+        const newAnalytics = new Analytics({
+            countryName,
+        });
+        await newAnalytics.save();
+        console.log(`Received countryName: ${countryName}`);
+        res.status(200).json({ message: "Analytics received successfully", countryName });
     } catch (e) {
         res.status(500).json({ message: 'Error while posting analyics name', error: e.message });
     }
