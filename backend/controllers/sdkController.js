5ddfbcac8f93ef14a0fcd6c2638490225d62b60b
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-03-25 19:03:58 +0530

    Improve logging format for received payload in sdkController

--- backend/controllers/sdkController.js
+++ backend/controllers/sdkController.js
@@ -8,7 +8,8 @@ exports.postCountryName = async (req, res) => {
         //     countryName,
         // });
         // await newAnalytics.save();
-        console.log(`Received countryName: ${payload}`);
+        console.log('Payload   Recieved :', JSON.stringify(payload, null, 2));
+        
         res.status(200).json({ message: "Analytics received successfully", payload });
     } catch (e) {
         res.status(500).json({ message: 'Error while posting analyics name', error: e.message });
