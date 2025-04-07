ae81b37ff7642c9d48830e048dfe455b119e3e24
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-04-07 17:06:00 +0530

    Remove updateAnalyticsStats function and update routes and Vercel configuration for cron job

--- backend/routes/sdkRouter.js
+++ backend/routes/sdkRouter.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { postAnalytics, getAnalytics} = require('../controllers/sdkController');
-const { updateAnalyticsStats } = require('../api/crone-jobs');
+const { postAnalytics, getAnalytics, updateAnalyticsStats} = require('../controllers/sdkController');
 
 const router = express.Router();
 
 
 router.post('/track', postAnalytics);
 router.get('/analytics/:siteId', getAnalytics);
-router.get('/update-all-analytics-stats/KIFJJI909HENO', updateAnalyticsStats);
+router.get('/update-all-analytics-stats', updateAnalyticsStats);
 
 module.exports = router;
