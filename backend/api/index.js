e860367cbcd0e8e81c13b7b4cc6e82b8212f234b
varsani-akshit <akshit.r.varsani@gmail.com>
2025-06-22 22:30:36 +0530

    Fix: Simplify to builds configuration and remove api directory

--- backend/api/index.js
+++
@@ -1,8 +0,0 @@
-// Serverless function handler for Vercel
-const app = require('../index');
-
-// Export the handler function for Vercel
-module.exports = (req, res) => {
-  // Handle the request using the Express app
-  return app(req, res);
-}; 
